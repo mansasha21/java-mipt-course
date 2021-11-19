e4123dd801d8ab768a2a1dcab4cf733b32b2b035
Alexandr Surzhikov <40700736+mansasha21@users.noreply.github.com>
2021-11-19 21:48:00 +0300

    add fixes hw4

--- hw4/src/main/java/Main.java
+++ hw4/src/main/java/Main.java
@@ -3,16 +3,31 @@ import ru.sber.CustomClassLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, MalformedURLException {
+
+        String[] names = {
+                "ru.sbt.java.tasks.Secret",
+                "ru.sbt.java.tasks.VeryStrangeSecret"
+        };
         URL[] urls = {
                 new URL("https://www.googleapis.com/drive/v3/files/1Hda8Kn9m96LBiNDUvBP7gTmrLustg70H?alt=media&key=AIzaSyCnyt2lgtvTEVvITi-mD7v0s49OaxLcEog"),
                 new URL("https://www.googleapis.com/drive/v3/files/1wG0bcva7AcA2v2TUEADYJFWCgSyL7KzN?alt=media&key=AIzaSyCnyt2lgtvTEVvITi-mD7v0s49OaxLcEog")
         };
-        CustomClassLoader loader = new CustomClassLoader(urls);
-        ((Runnable) loader.loadClass("ru.sbt.java.tasks.Secret").getConstructor().newInstance()).run();
-        ((Runnable) loader.loadClass("ru.sbt.java.tasks.VeryStrangeSecret").getConstructor().newInstance()).run();
+
+        Map<String, URL> nameUrlMap = new HashMap<>();
+        for (int i = 0; i < urls.length; i++) {
+            nameUrlMap.put(names[i], urls[i]);
+
+        }
+        CustomClassLoader loader = new CustomClassLoader(nameUrlMap);
+        for (String name : names) {
+            ((Runnable) loader.loadClass(name).getConstructor().newInstance()).run();
+        }
     }
 }
--- hw4/src/main/java/ru/sber/CustomClassLoader.java
+++ hw4/src/main/java/ru/sber/CustomClassLoader.java
@@ -2,60 +2,77 @@ package ru.sber;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomClassLoader extends ClassLoader {
 
-    private URL[] urls;
+    private Map<String, byte[]> preparedClasses;
 
-    public CustomClassLoader(URL[] urls) {
-        this.urls = urls;
+    public CustomClassLoader(Map<String, URL> nameUrlMap) {
+        preparedClasses = new HashMap<>();
+        for (String name : nameUrlMap.keySet()) {
+            preparedClasses.put(name, loadClassFromURL(nameUrlMap.get(name), name));
+        }
     }
 
     @Override
-    public Class findClass(String name) {
-        byte[] b = new byte[0];
-        try {
-            for (URL url : urls) {
-                b = loadClassFromURL(url, name);
-                if (b == null) {
-                    continue;
-                } else {
-                    break;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public Class findClass(String name) throws ClassNotFoundException {
+        var b = preparedClasses.getOrDefault(name, null);
+
+        if (b == null) {
+            throw new ClassNotFoundException();
         }
+
         return defineClass(name, b, 0, b.length);
     }
 
-    private byte[] loadClassFromURL(URL url, String name) throws IOException {
-        var urlCon = url.openConnection();
-        var inputStream = urlCon.getInputStream();
+    private byte[] loadClassFromURL(URL url, String name) {
+        URLConnection urlCon = null;
+        InputStream inputStream = null;
+        try {
+            urlCon = url.openConnection();
+            inputStream = urlCon.getInputStream();
+        } catch (IOException e) {
+            return null;
+        }
+
         byte[] buffer;
         ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        int nextValue = 0;
+
+        int nextValue;
         StringBuilder nameBuilder = new StringBuilder();
-        int i = 0;
+
         try {
             while ((nextValue = inputStream.read()) != -1) {
-                if (i < name.length()) {
-                    i += 1;
-                    nameBuilder.append((char) (nextValue - 1));
-                    continue;
-                }
-                if (!nameBuilder.toString().equals(name)) {
-                    return null;
-                }
-                byteStream.write(nextValue - 1);
+                if (nameBuilder.length() < name.length()) nameBuilder.append(decodeChar(nextValue));
+                else byteStream.write(nextValue - 1);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         buffer = byteStream.toByteArray();
+
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            byteStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return buffer;
     }
 
+    private char decodeChar(int b) {
+        return (char) (b - 1);
+    }
 
 }
